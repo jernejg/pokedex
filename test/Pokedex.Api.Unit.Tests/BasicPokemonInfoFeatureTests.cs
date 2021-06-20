d1efddd05eb0b72da9572a0d34ee1325133d233e
jernejg <jernej.goricki@gmail.com>
2021-06-20 23:08:02 +0100

    Configuration of test projects and unit test placeholders added

---
+++ test/Pokedex.Api.Unit.Tests/BasicPokemonInfoFeatureTests.cs
@@ -0,0 +1,50 @@
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Pokedex.Unit.Tests
+{
+	public class BasicPokemonInfoFeatureTests
+	{
+		[Fact]
+		public async Task Happy_Path_Test()
+		{
+			Assert.True(false);
+		}
+
+		[Fact]
+		public async Task Specie_Not_Found_Exception_Is_Raised_When_Pokemon_Doesnt_Exist_Test()
+		{
+
+		}
+
+		[Fact]
+		public async Task Pokemon_Service_Exception_Is_Raised_When_Info_Cant_Be_Retrieved_Test()
+		{
+
+		}
+
+		[Fact]
+		public async Task Pokemon_Service_Exception_Is_Raised_If_Specie_Is_Missing_English_Description_Test()
+		{
+
+		}
+
+		[Fact]
+		public async Task Pokemon_Service_Exception_Is_Raised_If_Specie_Is_Missing_English_Description2_Test()
+		{
+
+		}
+
+		[Fact]
+		public async Task Pokemon_Service_Exception_Is_Raised_If_Specie_Is_Missing_Habitat_Information_Test()
+		{
+
+		}
+
+		[Fact]
+		public async Task Pokemon_Service_Exception_Is_Raised_If_Specie_Is_Missing_Habitat_Information2_Test()
+		{
+
+		}
+	}
+}
---
+++ test/Pokedex.Api.Unit.Tests/FunPokemonInfoFeatureTests.cs
@@ -0,0 +1,53 @@
+﻿using System.Threading.Tasks;
+using Xunit;
+
+namespace Pokedex.Unit.Tests
+{
+	public class FunPokemonInfoFeatureTests
+	{
+		[Fact]
+		public async Task Happy_Path_Test()
+		{
+
+		}
+		[Fact]
+		public async Task Only_Yoda_Translator_Is_Used_For_Cave_Pokemons_Test()
+		{
+
+		}
+
+		[Fact]
+		public async Task Only_Yoda_Translator_Is_Used_For_Legendary_Pokemons_Test()
+		{
+
+		}
+		[Fact]
+		public async Task Only_Yoda_Translator_Is_Used_For_Legendary_And_Cave_Pokemons_Test()
+		{
+
+		}
+		[Fact]
+		public async Task Only_Shakespeare_Translator_Is_Used_For_Non_Cave_And_Non_Legendary_Pokemons_Test()
+		{
+
+		}
+
+		[Fact]
+		public async Task Use_Original_Pokemon_Description_If_Translation_Can_Not_Be_Retrieved_Test()
+		{
+
+		}
+
+		[Fact]
+		public async Task Translation_Service_Exception_Is_Raised_When_Translation_Cant_Be_Retrieved_Test()
+		{
+
+		}
+
+		[Fact]
+		public async Task Translation_Service_Exception_Is_Raised_When_Translation_Response_Does_Not_Indicate_Success_Test()
+		{
+
+		}
+	}
+}
