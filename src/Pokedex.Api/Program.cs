f23e088b391692906fd7a59e9814f1421a84b482
jernejg <jernej.goricki@gmail.com>
2021-06-20 23:22:05 +0100

    Configure Logger

--- src/Pokedex.Api/Program.cs
+++ src/Pokedex.Api/Program.cs
@@ -1,11 +1,7 @@
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Serilog;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Pokedex.Api
 {
@@ -13,11 +9,29 @@ namespace Pokedex.Api
 	{
 		public static void Main(string[] args)
 		{
-			CreateHostBuilder(args).Build().Run();
+			Log.Logger = new LoggerConfiguration()
+			  .Enrich.FromLogContext()
+			  .WriteTo.Console()
+			  .CreateLogger();
+
+			try
+			{
+				Log.Information("Starting up...");
+				CreateHostBuilder(args).Build().Run();
+			}
+			catch (Exception ex)
+			{
+				Log.Fatal(ex, "Application start-up failed");
+			}
+			finally
+			{
+				Log.CloseAndFlush();
+			}
 		}
 
 		public static IHostBuilder CreateHostBuilder(string[] args) =>
 			Host.CreateDefaultBuilder(args)
+				.UseSerilog()
 				.ConfigureWebHostDefaults(webBuilder =>
 				{
 					webBuilder.UseStartup<Startup>();
