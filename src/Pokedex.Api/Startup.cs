2b7f0cd323008121b5bacc7167994840d4ea3df0
jernejg <jernej.goricki@gmail.com>
2021-06-20 23:19:25 +0100

    Configure as web.api project and add infrastructure libraries
    - Automapper
    - MediatR
    - Serilog
    - Problem Details middleware

--- src/Pokedex.Api/Startup.cs
+++ src/Pokedex.Api/Startup.cs
@@ -1,38 +1,51 @@
+using Hellang.Middleware.ProblemDetails;
+using MediatR;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Reflection;
 
 namespace Pokedex.Api
 {
 	public class Startup
 	{
+		public IConfiguration Configuration { get; }
+		private readonly IWebHostEnvironment _env;
+
+		public Startup(IConfiguration configuration, IWebHostEnvironment env)
+		{
+			Configuration = configuration;
+			_env = env;
+		}
+
 		// This method gets called by the runtime. Use this method to add services to the container.
 		// For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
 		public void ConfigureServices(IServiceCollection services)
 		{
+			services.AddProblemDetails(setup =>
+			{
+				setup.IncludeExceptionDetails = (ctx, exception) => _env.IsDevelopment();
+			});
+			services.AddMediatR(Assembly.GetExecutingAssembly());
+			services.AddAutoMapper(Assembly.GetExecutingAssembly());
+			services.AddControllers();
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
 		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
 		{
-			if (env.IsDevelopment())
-			{
-				app.UseDeveloperExceptionPage();
-			}
-
+			app.UseProblemDetails();
+			app.UsePathBase(new PathString("/api"));
 			app.UseRouting();
-
+			app.UseStatusCodePages();
 			app.UseEndpoints(endpoints =>
 			{
-				endpoints.MapGet("/", async context =>
+				app.UseEndpoints(endpoints =>
 				{
-					await context.Response.WriteAsync("Hello World!");
+					endpoints.MapControllers();
 				});
 			});
 		}
